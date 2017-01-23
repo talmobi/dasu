49e5b20f2a9d55e11696783aac319d4b54778fb4
Christian Talmo <talmo.christian@gmail.com>
2017-01-23 22:35:49 +0200

    corretctly use opts.protocol if available

--- dasu.js
+++ dasu.js
@@ -52,9 +52,16 @@ if (typeof window !== 'undefined' && typeof window.XMLHttpRequest !== 'undefined
   // node's http library within exported bundles
   var require_ = require
   var http = require_('http')
+  var https = require_('https')
 
   _request = function (opts, dataString, callback) {
-    var req = http.request(opts, function (res) {
+    opts = opts || {}
+    opts.protocol = opts.protocol || 'http'
+    // console.log(opts.protocol)
+    var _h = http
+    if (opts.protocol && opts.protocol.indexOf('https') !== -1) _h = https
+    if (opts.protocol[opts.protocol.length - 1] !== ':') opts.protocol += ':'
+    var req = _h.request(opts, function (res) {
       var buffer = ''
       res.on('data', function (chunk) {
         buffer += chunk
@@ -124,6 +131,7 @@ function request (params, done) {
   }
 
   var opts = {
+    protocol: params.protocol,
     hostname: params.hostname,
     port: params.port,
     path: params.path, // attach root path
