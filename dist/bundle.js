f12239d8a048e94019a7b156132006a78ccbdfab
Christian Talmo <talmo.christian@gmail.com>
2017-09-24 02:11:34 +0300

    nodejs error convention of 'null' instead of 'undefined', linted with spacestandard, shrinkwrap

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,25 +7,25 @@
   // basic streamlined xhr request for browser and server
 
   var _request = function () {
-    throw new Error('dasu: No request implemention specified in dasu.js')
+    throw new Error( 'dasu: No request implemention specified in dasu.js' )
   }
 
-  if (typeof window !== 'undefined' && typeof window.XMLHttpRequest !== 'undefined') {
+  if ( typeof window !== 'undefined' && typeof window.XMLHttpRequest !== 'undefined' ) {
     // use XMLHttpRequest
 
-    _request = function (opts, dataString, callback) {
+    _request = function ( opts, dataString, callback ) {
       var req = new window.XMLHttpRequest()
 
-      opts.protocol = opts.protocol || (window.location.protocol) || 'http'
-      if (opts.protocol[opts.protocol.length - 1] !== ':') opts.protocol += ':'
+      opts.protocol = opts.protocol || ( window.location.protocol ) || 'http'
+      if ( opts.protocol[ opts.protocol.length - 1 ] !== ':' ) opts.protocol += ':'
       // opts.host = opts.host || window.location.host
       opts.hostname = opts.hostname || window.location.hostname
 
       var defaultPort = 80
 
       // default port based off of protocol
-      if (opts.protocol) {
-        if (opts.protocol.indexOf('https') !== -1) {
+      if ( opts.protocol ) {
+        if ( opts.protocol.indexOf( 'https' ) !== -1 ) {
           defaultPort = 443
         } else {
           defaultPort = 80
@@ -35,34 +35,34 @@
       // if requesting current domain, assume by default the same port
       var ohn = opts.hostname.toLowerCase().trim()
       var whn = window.location.hostname.toLowerCase().trim()
-      if (ohn.indexOf(whn) !== -1) defaultPort = window.location.port || defaultPort
+      if ( ohn.indexOf( whn ) !== -1 ) defaultPort = window.location.port || defaultPort
 
       opts.port = opts.port || defaultPort
 
-      var origin = opts.protocol + '//' + (opts.hostname + (opts.port ? (':' + opts.port) : ''))
+      var origin = opts.protocol + '//' + ( opts.hostname + ( opts.port ? ( ':' + opts.port ) : '' ) )
       // XMLHttpRequest takes a complete url (not host and path separately)
       var url = origin + opts.path
-      console.log('dasu: url [' + url + ']')
-      req.open(opts.method, url, true)
+      console.log( 'dasu: url [' + url + ']' )
+      req.open( opts.method, url, true )
 
       req.onload = function () {
-        callback(undefined, req, req.responseText)
+        callback( null, req, req.responseText )
       }
 
       req.onerror = function () {
-        callback(req.responseText || 'dasu.js XMLHttpRequest error')
+        callback( req.responseText || 'dasu.js XMLHttpRequest error' )
       }
 
       // attach headers to the request
       // console.log(opts.headers)
-      var headerKeys = Object.keys(opts.headers)
-      for (var i = 0; i < headerKeys.length; i++) {
-        var key = headerKeys[i]
-        var value = opts.headers[key]
-        req.setRequestHeader(key, value)
+      var headerKeys = Object.keys( opts.headers )
+      for ( var i = 0; i < headerKeys.length; i++ ) {
+        var key = headerKeys[ i ]
+        var value = opts.headers[ key ]
+        req.setRequestHeader( key, value )
         // console.log("set header: %s, to: %s", key, value)
       }
-      req.send(dataString)
+      req.send( dataString )
 
       // return minimal api to abort
       return {
@@ -75,33 +75,33 @@
     // use this require hack so that bundlers don't automatically bundle
     // node's http library within exported bundles
     var require_ = require
-    var http = require_('http')
-    var https = require_('https')
+    var http = require_( 'http' )
+    var https = require_( 'https' )
 
-    _request = function (opts, dataString, callback) {
+    _request = function ( opts, dataString, callback ) {
       opts = opts || {}
       opts.protocol = opts.protocol || 'http'
       var _h = http
-      if (opts.protocol && opts.protocol.indexOf('https') !== -1) _h = https
-      if (opts.protocol[opts.protocol.length - 1] !== ':') opts.protocol += ':'
-      var req = _h.request(opts, function (res) {
+      if ( opts.protocol && opts.protocol.indexOf( 'https' ) !== -1 ) _h = https
+      if ( opts.protocol[ opts.protocol.length - 1 ] !== ':' ) opts.protocol += ':'
+      var req = _h.request( opts, function ( res ) {
         var buffer = ''
-        res.on('data', function (chunk) {
+        res.on( 'data', function ( chunk ) {
           buffer += chunk
-        })
+        } )
 
-        res.on('end', function () {
-          callback(undefined, res, buffer)
-        })
-      })
+        res.on( 'end', function () {
+          callback( null, res, buffer )
+        } )
+      } )
 
-      req.on('error', function (err) {
+      req.on( 'error', function ( err ) {
         // console.error("error: " + err)
-        callback(err)
-      })
+        callback( err )
+      } )
 
       // console.log('rest: sending: ' + dataString)
-      req.write(dataString)
+      req.write( dataString )
       req.end()
 
       // return minimal api to abort
@@ -113,40 +113,40 @@
     }
   }
 
-  function request (params, done) {
+  function request ( params, done ) {
     var contentType = ''
-    var data = (params.data || params.json) || ''
+    var data = ( params.data || params.json ) || ''
     var dataString = ''
-    switch (typeof data) {
+    switch ( typeof data ) {
       case 'object':
-        dataString = JSON.stringify(data)
+        dataString = JSON.stringify( data )
         contentType = 'application/json'
         break
       case 'string':
-        if (data.length > 1 && (data[0] === '{' || data[0] === '[')) {
+        if ( data.length > 1 && ( data[ 0 ] === '{' || data[ 0 ] === '[' ) ) {
           try { // could be json
-            JSON.parse(data) // throws error on fail
-            if (console && console.warn) console.warn('dasu: Sending data that may be JSON as text/plain')
-          } catch (err) {} // text was not parsed as json, ignore and assume text/plain
+            JSON.parse( data ) // throws error on fail
+            if ( console && console.warn ) console.warn( 'dasu: Sending data that may be JSON as text/plain' )
+          } catch ( err ) {} // text was not parsed as json, ignore and assume text/plain
         }
         dataString = data
         contentType = 'text/plain'
         break
       default: // try coercion as a last resort
-        dataString = ('' + data)
+        dataString = ( '' + data )
         contentType = 'text/plain'
     }
     // console.log('dataString: ' + dataString)
 
     // console.log("rest: contentType: " + contentType)
 
-    params = Object.assign({}, params)
+    params = Object.assign( {}, params )
     delete params.data
 
     // try to add content-type if it doesn't exist
-    params.headers = Object.assign({}, {
+    params.headers = Object.assign( {}, {
       'content-type': contentType
-    }, params.headers || {})
+    }, params.headers || {} )
 
     // console.log("rest: headers: " + JSON.stringify(params.headers))
 
@@ -156,7 +156,7 @@
     // default path
     params.path = params.path || '/'
     // people often emit leading '/' - so support it
-    if (params.path[0] !== '/') {
+    if ( params.path[ 0 ] !== '/' ) {
       params.path = '/' + params.path
     }
 
@@ -170,37 +170,37 @@
     }
 
     // used XMLHttpRequest if availalb, else nodejs http library
-    return _request(opts, dataString, function (err, res, body) {
-      if (err || res === undefined) {
-        done(err)
+    return _request( opts, dataString, function ( err, res, body ) {
+      if ( err || res === undefined ) {
+        done( err )
       } else {
         // homogenize response headers
-        if (!res.getResponseHeader && res.headers) {
-          res.getResponseHeader = function (header) {
-            return res.headers[header]
+        if ( !res.getResponseHeader && res.headers ) {
+          res.getResponseHeader = function ( header ) {
+            return res.headers[ header ]
           }
         }
-        if (res.getAllResponseHeaders && !res.headers) {
+        if ( res.getAllResponseHeaders && !res.headers ) {
           res.headers = res.getAllResponseHeaders()
         }
 
         // homogenize response status
-        if (res.status === undefined) {
+        if ( res.status === undefined ) {
           res.status = res.statusCode
         } else {
           res.statusCode = res.status
         }
 
-        done(undefined, res, body)
+        done( undefined, res, body )
       }
-    })
+    } )
   }
 
   var client = {
-    xhr: function (params, done) {
-      request(params, function (err, res, body) {
-        done(err, body)
-      })
+    xhr: function ( params, done ) {
+      request( params, function ( err, res, body ) {
+        done( err, body )
+      } )
     },
     req: request
   }
