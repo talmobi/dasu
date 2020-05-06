ca7335fabccd513fa8f3a3766b7e2e6e7efbc833
Christian Talmo <talmo.christian@gmail.com>
2020-05-06 11:11:56 +0300

    fix: handle non-existing content-type header

--- dasu.js
+++ dasu.js
@@ -172,11 +172,15 @@ function nodeRequest ( opts, dataString, callback ) {
     }
 
     var contentType = res.headers[ 'content-type' ]
-    if ( contentType.indexOf( 'text/' ) >= 0 ) {
-      if ( contentType.indexOf( 'ascii' ) >= 0 ) {
-        stream.setEncoding( 'ascii' )
+    if ( contentType ) {
+      if ( contentType.indexOf( 'text/' ) >= 0 ) {
+        if ( contentType.indexOf( 'ascii' ) >= 0 ) {
+          stream.setEncoding( 'ascii' )
+        } else {
+          stream.setEncoding( 'utf8' )
+        }
       } else {
-        stream.setEncoding( 'utf8' )
+        stream.setEncoding( 'binary' )
       }
     } else {
       stream.setEncoding( 'binary' )
